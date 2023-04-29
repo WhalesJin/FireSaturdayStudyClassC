a06cc027a19c230ce585f5e30f8f8a92868564f1
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-04-29 15:42:02 +0900

    1주차 토요스터디 - optional by EtialMoon

--- week1_optional/optional/main.swift
+++ week1_optional/optional/main.swift
@@ -1,96 +1,72 @@
 //
 //  main.swift
-//  optional
+//  playground
 //
-//  Created by DasanKim, EtialMoon, Kobe, Whales on 2023/04/29.
-//  with Gundy
+//  Created by 조호준 on 2023/04/29.
+//
 
-import Foundation
 
 // MARK: - STEP 1
+let price: Int = 1000
+var budget: Int = 2000
 var productsList: [String?] = ["볼펜", "텀블러", "다이어리", "에코백", "머그컵", "후드집업"]
 
-// Binding - if let
-func usingIf() {
-for index in 0..<productsList.count {
-    if let goods: String = productsList[index] {
-        print("\(index)번 상품은 \(goods)입니다.")
-    }
-}
-}
-
-// Binding - guard let
-func usingGuard() {
-    for index in 0..<productsList.count {
-        guard let goods: String = productsList[index] else {
-            print("\(index)번 상품은 상품이 없습니다.")
-            continue
-        }
-        print("\(index)번 상품은 \(goods)입니다.")
-    }
-}
-
-// Binding - switch
-func usingSwitch() {
-    for index in 0..<productsList.count {
-        let goodsName: String? = productsList[index]
-        
-        switch goodsName {
-        case .none:
-            print("상품이 없습니다.")
-        case .some(let goods):
-            print("\(index)번 상품은 \(goods)입니다.")
-        }
-    }
-}
-
-// Binding - while let
-func usingWhile() {
-    var index = 0
-
-    while index < productsList.count,
-          let goods = productsList[index] {
-        print("\(index)번 상품은 \(goods)입니다.")
-        index += 1
-    }
-}
-
-// Binding 외 - compactMap, enumerated()
-let unwrappedProductsList = productsList.compactMap { $0 }
-func usingCompactMap() {
-    for (index, product) in unwrappedProductsList.enumerated() {
+// if let
+for (index, product) in productsList.enumerated() {
+    if let product {
         print("\(index)번 상품은 \(product)입니다.")
     }
 }
+// guard let
+for (index, product) in productsList.enumerated() {
+    guard let product else {
+        continue
+    }
+    print("\(index)번 상품은 \(product)입니다.")
+}
+// compactMap
+let unwrappedProductsList = productsList.compactMap { $0 }
+for (index, product) in unwrappedProductsList.enumerated() {
+    print("\(index)번 상품은 \(product)입니다.")
+}
 
-// Binding 외 - nil Coalescing Operator(nil 병합 연산자)
-func usingNilCoalescingOperator() {
-    for indexNumber in 0...productsList.count - 1 {
-        let goods: String = productsList[indexNumber] ?? "no 상품"
-        print("\(indexNumber)번 상품은 \(goods)입니다.")
+// ??? while let을 시도했으나 for문 안에 사용하게 되어 의미가 없지 않나
+for (index, product) in productsList.enumerated() {
+    while let unwrappedProduct = product {
+        print("\(index)번 상품은 \(unwrappedProduct)입니다.")
+        break
     }
 }
 
-//usingNilCoalescingOperator()
+// !!! while let Gundy가 알려주신 방법
+var index = 0
+while index < productsList.count,
+      let goods = productsList[index] {
+    print("\(index)번 상품은 \(goods)입니다.")
+    index += 1
+}
+index = 0
+while let goods = productsList[index] {
+    print("\(index)번 상품은 \(goods)입니다.")
+    index += 1
+    if index == productsList.count { break }
+}
+
 
 // MARK: - STEP 2
-
-let price: Int = 1000
-var budget: Int = 2000
-
 func buy(productNumber: Int) -> () {
     guard let product = productsList[productNumber] else {
         print("\(productNumber)번 상품은 품절입니다.\n")
         return
     }
-
+    
     productsList[productNumber] = nil
     budget -= price
     print("""
     \(productNumber)번 상품 \(product)를 구매하였습니다.
     잔액이 \(budget)원입니다.\n
     """)
-
+    
     return
 }
 
@@ -105,21 +81,17 @@ func showProductsList() {
     print()
 }
 
-func inputOrderNumber() {
-    while budget >= price {
-        showProductsList()
-        
-        print("구매할 상품 번호를 입력해 주세요 : ", terminator: "")
-        
-        guard let inputProductNumber = readLine(),
-              let inputProductNumber = Int(inputProductNumber),
-              0...5 ~= inputProductNumber else {
-            print("잘못된 입력입니다.")
-            continue
-        }
-        
-        buy(productNumber: inputProductNumber)
+while budget >= price {
+    showProductsList()
+    print("구매할 상품 번호를 입력해 주세요 : ", terminator: "")
+    
+    guard let inputProductNumber = readLine(),
+          let inputProductNumber = Int(inputProductNumber),
+          0...5 ~= inputProductNumber else {
+        print("잘못된 입력입니다.")
+        continue
     }
+    
+    buy(productNumber: inputProductNumber)
 }
 
-inputOrderNumber()
