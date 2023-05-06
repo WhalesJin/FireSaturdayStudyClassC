be4c9d9b4eadb8ee58cc311cda5988cbf789afe8
DasanKim <tempzipp@gmail.com>
2023-05-06 17:39:15 +0900

    잘못 덮어쓰여진 파일 되돌림

--- week1_optional/optional/main.swift
+++ week1_optional/optional/main.swift
@@ -1,125 +1,99 @@
-//
-//  main.swift
-//  optional
-//
-//  Created by DasanKim, EtialMoon, Kobe, Whales on 2023/04/29.
-//  with Gundy
-
-import Foundation
-
-// MARK: - STEP 1
+/*
+ 
+// STEP1
+let price: Int = 900
+var budget: Int = 3000
 var productsList: [String?] = ["볼펜", "텀블러", "다이어리", "에코백", "머그컵", "후드집업"]
 
-// Binding - if let
-func usingIf() {
-for index in 0..<productsList.count {
-    if let goods: String = productsList[index] {
-        print("\(index)번 상품은 \(goods)입니다.")
-    }
-}
+//compactMap
+let products = productsList.compactMap{ $0 }
+for index in 0..<products.count {
+    print("\(index)번 상품은 \(products[index]) 입니다.")
 }
 
-// Binding - guard let
-func usingGuard() {
-    for index in 0..<productsList.count {
-        guard let goods: String = productsList[index] else {
-            print("\(index)번 상품은 상품이 없습니다.")
-            continue
-        }
-        print("\(index)번 상품은 \(goods)입니다.")
-    }
+//compactMap & enumerated()
+let products = productsList.compactMap{ $0 }
+
+for (index, product) in products.enumerated() {
+    print("\(index)번 상품은 \(product) 입니다.")
 }
 
-// Binding - switch
-func usingSwitch() {
-    for index in 0..<productsList.count {
-        let goodsName: String? = productsList[index]
-        
-        switch goodsName {
-        case .none:
-            print("상품이 없습니다.")
-        case .some(let goods):
-            print("\(index)번 상품은 \(goods)입니다.")
-        }
+//if let
+for (index, product) in productsList.enumerated() {
+    if let product = product {
+        print("\(index)번 상품은 \(product) 입니다.")
+    } else {
+        print("\(index)번은 상품이 없습니다.")
+        continue
     }
 }
 
-// Binding - while let
-func usingWhile() {
-    var index = 0
-
-    while index < productsList.count,
-          let goods = productsList[index] {
-        print("\(index)번 상품은 \(goods)입니다.")
-        index += 1
+//guard let
+for (index, product) in productsList.enumerated() {
+    guard let product = product else {
+        print("\(index)번은 상품이 없습니다.")
+        continue
     }
+    print("\(index)번 상품은 \(product) 입니다.")
 }
 
-// Binding 외 - compactMap, enumerated()
-let unwrappedProductsList = productsList.compactMap { $0 }
-func usingCompactMap() {
-    for (index, product) in unwrappedProductsList.enumerated() {
-        print("\(index)번 상품은 \(product)입니다.")
-    }
+//nil coalescing
+for (index, product) in productsList.enumerated() {
+    //let test = product ?? ""
+    print("\(index)번 상품은 \(product ?? "") 입니다.")
 }
 
-// Binding 외 - nil Coalescing Operator(nil 병합 연산자)
-func usingNilCoalescingOperator() {
-    for indexNumber in 0...productsList.count - 1 {
-        let goods: String = productsList[indexNumber] ?? "no 상품"
-        print("\(indexNumber)번 상품은 \(goods)입니다.")
-    }
+//while let - 연결리스트 구현할 때 주로 쓰임.
+var index = 0
+while index < productsList.count, let product = productsList[index] {
+    print("\(index)번 상품은 \(product) 입니다.")
+    index += 1
 }
 
-//usingNilCoalescingOperator()
-
-// MARK: - STEP 2
-
-let price: Int = 1000
-var budget: Int = 2000
-
-func buy(productNumber: Int) -> () {
-    guard let product = productsList[productNumber] else {
-        print("\(productNumber)번 상품은 품절입니다.\n")
-        return
-    }
-
-    productsList[productNumber] = nil
-    budget -= price
-    print("""
-    \(productNumber)번 상품 \(product)를 구매하였습니다.
-    잔액이 \(budget)원입니다.\n
-    """)
-
-    return
+//while let2
+var index = 0
+while let goods = productsList[index] {
+    print(goods)
+    index += 1
+    if index == productsList.count { break }
 }
+*/
 
-func showProductsList() {
+// STEP2
+let price: Int = 900
+var budget: Int = 3000
+var productsList: [String?] = ["볼펜", "텀블러", "다이어리", "에코백", "머그컵", "후드집업"]
+
+func printProduct() {
     for (index, product) in productsList.enumerated() {
-        if let product {
-            print("\(index)번 상품은 \(product)입니다.")
-        } else {
-            print("\(index)번 상품은 품절입니다.")
-        }
-    }
-    print()
-}
-
-func inputOrderNumber() {
-    while budget >= price {
-        showProductsList()
-        
-        print("구매할 상품 번호를 입력해 주세요 : ", terminator: "")
-        
-        guard let inputProductNumber = readLine(),
-              let inputProductNumber = Int(inputProductNumber),
-              0...5 ~= inputProductNumber else {
-            print("잘못된 입력입니다.")
+        guard let product = product else {
+            print("\(index)번 상품은 품절되었습니다.")
             continue
         }
-        
-        buy(productNumber: inputProductNumber)
+        print("\(index)번 상품은 \(product) 입니다.")
     }
 }
 
-inputOrderNumber()
+func buy(productNumber: Int) {
+    if budget >= price {
+        if let product = productsList[productNumber] {
+            print("\(productNumber)번 \(product)가 구매되었습니다.")
+            productsList[productNumber] = nil
+            budget -= price
+        }
+        else {
+            print("\(productNumber)번 상품은 품절되었습니다.")
+        }
+    } else {
+        print("잔액이 부족합니다.")
+        print("현재잔액: \(budget)")
+    }
+}
+
+buy(productNumber: 1)
+buy(productNumber: 1)
+buy(productNumber: 2)
+buy(productNumber: 3)
+buy(productNumber: 0)
+buy(productNumber: 5)
+//printProduct()
